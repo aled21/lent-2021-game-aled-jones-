e3d010a3f1e4d3f32902b3cc9f8ab82b78668e0b
aled21 <77404338+aled21@users.noreply.github.com>
2021-01-20 16:40:35 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,43 @@
-
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . 6 6 6 6 6 
+        . . . . . . . . . 6 6 7 7 7 7 8 
+        . . . . . . 8 8 8 7 7 8 8 6 8 8 
+        . . e e e e c 6 6 8 8 . 8 7 8 . 
+        . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+        e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+        e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+        e 2 e e 2 2 2 2 e e e e c 6 8 . 
+        c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+        . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+        . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+        . . . e c c e c 2 2 2 2 2 2 2 e 
+        . . . . . . . c 2 e e 2 2 e 2 c 
+        . . . . . . . c e e e e e e 2 c 
+        . . . . . . . . c e 2 2 2 2 c . 
+        . . . . . . . . . c c c c c . . 
+        `, mySprite, 200, 0)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . e e e . . . . . . . . 
+    . . . . e . . . e . . . . . . . 
+    . . . e . f . f . e . . . . . . 
+    . . . e . . . . . e . . . . . . 
+    . . . e . f . f . e . . . . . . 
+    . . . . e . f . e . . . . . . . 
+    . . . . . e e e . . . . . . . . 
+    . . . . . . e . . . . . . . . . 
+    . . . . . . e . . . . . . . . . 
+    . . . . e e e e e . . . . . . . 
+    . . . . . . e . . . . . . . . . 
+    . . . . . e . e . . . . . . . . 
+    . . . . e . . . e . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
